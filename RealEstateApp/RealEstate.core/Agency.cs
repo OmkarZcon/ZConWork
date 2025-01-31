acf4d0117d5680284b479c36b334cab589ccef3e
OmkarZcon <omkar.bate@zconsolutions.com>
2025-01-31 16:32:32 +0530

    Real Estate coree.

--- RealEstateApp/RealEstate.core/Agency.cs
+++ RealEstateApp/RealEstate.core/Agency.cs
@@ -6,14 +6,11 @@
         public string AgencyName { get; set; } = string.Empty;
         public string AgencyPincode { get; set; }
         public Guid BrandId { get; set; }
-        public bool IsDeleted { get; set; } = false; // Soft delete flag
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Audit column
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow; // Audit column
-
+       
         // Method to print agency details
         public void GetDetails()
         {
-            Console.WriteLine($"ID: {AgencyId}, Name: {AgencyName}, BrandId: {BrandId}, Pincode: {AgencyPincode}, IsDeleted: {IsDeleted}, CreatedAt: {CreatedAt}, UpdatedAt: {UpdatedAt}");
+            Console.WriteLine($"ID: {AgencyId}, Name: {AgencyName}, BrandId: {BrandId}, Pincode: {AgencyPincode}");
         }
     }
 }
--- RealEstateApp/RealEstate.core/FileLogger.cs
+++ RealEstateApp/RealEstate.core/FileLogger.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Configuration;
+﻿using System.Configuration;
 
 namespace RealEstate.core
 {
