e8ca768fa2d9e6693987483a150a2bb3f4618ee8
OmkarZcon <omkar.bate@zconsolutions.com>
2025-02-03 14:40:20 +0530

    Azure Table Storage

---
+++ RealEstateApp/AzureTableStorage.Functions/PropertyEntity.cs
@@ -0,0 +1,26 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+
+namespace AzureTableStorage.Functions
+{
+    public class PropertyEntity : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string Address { get; set; }
+        public double Price { get; set; }
+
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        public PropertyEntity() { }
+        public PropertyEntity(string propertyType, string propertyId, string address, double price)
+        {
+            PartitionKey = propertyType;
+            RowKey = propertyId;
+            Address = address;
+            Price = price;
+        }
+    }
+}
